6d5a69e77b8290dae075f2fb983334210a729ef1
unknown <kennethmckrola@gmail.com>
2021-09-25 14:12:00 -0600

    milestone 1 achieved

--- Adventure.java
+++ Adventure.java
@@ -1,6 +1,5 @@
 import java.util.Arrays;
 import java.util.Scanner;
-
 import java.lang.System;
 import java.util.HashMap;
 
@@ -11,14 +10,16 @@ public class Adventure {
      */
     static String[] inventory = new String[4];
     static boolean quitProgram = false; // changed to true when exit entered
+    static int xPos = 0; // create coordinate system for player
+    static int yPos = 0;
+    static String[] valid_commands = new String[3];
 
     public static void main(String[] args) {
-        int[][] position = new int[0][0]; // coordinates of player
 
-        String[] valid_commands = new String[3];
-        valid_commands[0] = "go";
-        valid_commands[1] = "inventory";
-        valid_commands[2] = "quit";
+        Adventure.valid_commands[0] = "go";
+        Adventure.valid_commands[1] = "inventory";
+        Adventure.valid_commands[2] = "quit";
+        // Adventure.valid_commands[3] = "help";
 
         Adventure.inventory[0] = "brass lantern";
         Adventure.inventory[1] = "rope";
@@ -50,12 +51,14 @@ public class Adventure {
         char firstChar = cm.charAt(0);
 
         if (firstChar == 'g') {
-            go(command[0]);
+            go(command[1].toLowerCase());
         } else if (firstChar == 'i') {
             printInventory();
         } else if (firstChar == 'q') {
             quit();
-        }
+        } // else if (firstChar == 'h') {
+          // help();
+          // }
     }
 
     public static void quit() {
@@ -63,7 +66,44 @@ public class Adventure {
     }
 
     public static void go(String direction) {
+        // User enters 'go' followed by direction
+        int x = Adventure.xPos;
+        int y = Adventure.yPos;
 
+        if (direction.equals("east")) {
+            if (x == 5) {
+                System.out.println("Can't move East!");
+            } else {
+                x++;
+                System.out.println("Current position: " + x + ", " + y);
+            }
+        } else if (direction.equals("west")) {
+            if (x == 0) {
+                System.out.println("Can't move West!");
+            } else {
+                x--;
+                System.out.println("Current position: " + x + ", " + y);
+            }
+        } else if (direction.equals("north")) {
+            if (y == 5) {
+                System.out.println("Can't move North!");
+            } else {
+                y++;
+                System.out.println("Current position: " + x + ", " + y);
+            }
+        } else if (direction.equals("south")) {
+            if (y == 0) {
+                System.out.println("Can't move South!");
+            } else {
+                y--;
+                System.out.println("Current position: " + x + ", " + y);
+            }
+        } else {
+            System.out.println("You can't go that direction");
+        }
+
+        Adventure.xPos = x;
+        Adventure.yPos = y;
     }
 
     public static void printInventory() {
@@ -71,6 +111,12 @@ public class Adventure {
             System.out.printf(inventory[i] + "\n");
         }
     }
+
+    // public static void help() {
+    // for (int i = 0; i < Adventure.valid_commands.length; i++) {
+    // System.out.print(valid_commands[i]);
+    // }
+    // }
 
     private static boolean check(String[] arr, String element) {
         String[] arrOfEl = element.split("\\s+");
